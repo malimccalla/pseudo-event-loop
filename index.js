b471f8f319eec71d6b071a26c851f490ae908e4a
Mali Michael <malimccalla@gmail.com>
2018-03-19 12:25:22 +0000

    Clustering

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hi there');
-});
+if (cluster.isMaster) {
+  // first time index.js is ran we will be in master mode
+  // running 'fork()' will generate a child or 'worker' instance
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  // Here the code will be executed in worker mode
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
 
-app.listen(8000);
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('Wow, that was fast');
+  });
+
+  app.listen(8000);
+}
